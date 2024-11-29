be796dd3f8884a4e91f4f982153ee54114f93730
hama <hama@hama.industries>
2024-11-28 22:44:15 -0800

    initial

---
+++ src/client/java/symbolics/division/modfest_credits/Modfest_creditsClient.java
@@ -0,0 +1,10 @@
+package symbolics.division.modfest_credits;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Modfest_creditsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/symbolics/division/modfest_credits/Modfest_creditsDataGenerator.java
@@ -0,0 +1,11 @@
+package symbolics.division.modfest_credits;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Modfest_creditsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
